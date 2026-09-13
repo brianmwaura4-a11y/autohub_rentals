2fead3489955aeb6f6e02cfb96bdfa78eb4a813e
Brian Mwaura <brian.mwaura4@student.moringaschool.com>
2026-09-13 08:04:24 +0300

    built the main CLI with argparse to allow use of the system from terminal

---
+++ car_rental/cli/main.py
@@ -0,0 +1,193 @@
+import argparse
+
+from car_rental.cli.auth_commands import (
+    register_command,
+    login_command
+)
+from car_rental.cli.car_commands import (
+    list_cars_command,
+    add_car_command
+)
+from car_rental.cli.rental_commands import (
+    rent_car_command,
+    return_car_command
+)
+
+
+def create_parser():
+
+    parser = argparse.ArgumentParser(
+        prog="car-rental",
+        description="Car Rental Management System"
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command"
+    )
+
+    # Register
+    register_parser = subparsers.add_parser(
+        "register",
+        help="Register a new user"
+    )
+
+    register_parser.add_argument(
+        "username",
+        help="Username"
+    )
+
+    register_parser.add_argument(
+        "password",
+        help="Password"
+    )
+
+    register_parser.add_argument(
+        "--role",
+        default="Customer",
+        choices=[
+            "Customer",
+            "Rental Staff",
+            "Administrator",
+            "Maintenance"
+        ],
+        help="User role"
+    )
+
+    # Login
+    login_parser = subparsers.add_parser(
+        "login",
+        help="Login to the system"
+    )
+
+    login_parser.add_argument(
+        "username",
+        help="Username"
+    )
+
+    login_parser.add_argument(
+        "password",
+        help="Password"
+    )
+
+    # Cars
+    subparsers.add_parser(
+        "cars",
+        help="List available cars"
+    )
+
+    # Add car
+    add_car_parser = subparsers.add_parser(
+        "add-car",
+        help="Add a new car"
+    )
+
+    add_car_parser.add_argument(
+        "make"
+    )
+
+    add_car_parser.add_argument(
+        "model"
+    )
+
+    add_car_parser.add_argument(
+        "year",
+        type=int
+    )
+
+    add_car_parser.add_argument(
+        "registration_number"
+    )
+
+    add_car_parser.add_argument(
+        "daily_rate",
+        type=float
+    )
+
+    # Rent
+    rent_parser = subparsers.add_parser(
+        "rent",
+        help="Rent a car"
+    )
+
+    rent_parser.add_argument(
+        "user_id",
+        type=int
+    )
+
+    rent_parser.add_argument(
+        "car_id",
+        type=int
+    )
+
+    rent_parser.add_argument(
+        "start_date"
+    )
+
+    rent_parser.add_argument(
+        "end_date"
+    )
+
+    # Return
+    return_parser = subparsers.add_parser(
+        "return",
+        help="Return a rented car"
+    )
+
+    return_parser.add_argument(
+        "rental_id",
+        type=int
+    )
+
+    return parser
+
+
+def main():
+
+    parser = create_parser()
+
+    args = parser.parse_args()
+
+    if args.command == "register":
+        register_command(
+            args.username,
+            args.password,
+            args.role
+        )
+
+    elif args.command == "login":
+        login_command(
+            args.username,
+            args.password
+        )
+
+    elif args.command == "cars":
+        list_cars_command()
+
+    elif args.command == "add-car":
+        add_car_command(
+            args.make,
+            args.model,
+            args.year,
+            args.registration_number,
+            args.daily_rate
+        )
+
+    elif args.command == "rent":
+        rent_car_command(
+            args.user_id,
+            args.car_id,
+            args.start_date,
+            args.end_date
+        )
+
+    elif args.command == "return":
+        return_car_command(
+            args.rental_id
+        )
+
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
